8caec7bd1d51968cfd11d95fb0cc08fb1d8116e3
Ramesh Shrestha <fx_ra@hotmail.com>
2024-02-24 09:02:32 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 const baseURL = process.env.REACT_APP_SERVER_URI;
+const socketURL = process.env.REACT_APP_SOCKET_URI
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : `${baseURL}/chat`;
+const URL = process.env.NODE_ENV === 'production' ? undefined : `${socketURL}`;
 
 export const socket = io(URL,{autoConnect:false});
